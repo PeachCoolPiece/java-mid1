f0abf5f0d17158d046e83365225ecb957987a282
PeachCoolPiece <121959157+PeachCoolPiece@users.noreply.github.com>
2024-03-28 22:05:36 +0900

    오브젝트 다운캐스팅

---
+++ src/lang/object/LangMain.java
@@ -0,0 +1,8 @@
+package lang.object;
+
+public class LangMain {
+    
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
---
+++ src/lang/object/Parent.java
@@ -0,0 +1,9 @@
+package lang.object;
+
+// 부모가 없으면 묵시적 으로 Object 클래스 를 상속받는다
+public class Parent {
+    
+    public void parentMethod() {
+        System.out.println("Parent.parentMethod");
+    }
+}
